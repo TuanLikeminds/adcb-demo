a10d7b12e5a22126c084defe7d621c3136ae3399
saadousman <tuan@likemindsconsulting.com>
2024-09-16 16:30:27 +0530

    parameterized stuff

--- .github/workflows/workflow-steps.sh
+++ .github/workflows/workflow-steps.sh
@@ -12,6 +12,7 @@ ACR_REGISTRY_NAME=$ACR_REGISTRY_NAME
 PING_PRODUCT_ACR_REGISTRY_URL=$PING_PRODUCT_ACR_REGISTRY_URL
 BASE_IMAGE_TAG=$BASE_IMAGE_TAG
 BASE_IMAGE_REPOSITORY=$BASE_IMAGE_REPOSITORY
+PRODUCT_NAME=$PRODUCT_NAME
 
 apply_overlays() {
   echo "Applying overlays..."
@@ -67,21 +68,25 @@ deploy_pingdirectory_dev() {
   #s
   echo "Azure login"
   az login --service-principal -u "6fca71cf-2e16-48fd-9c52-cb1d0f72b898" -p "r6U8Q~JYsjXxkjeILYVegugmEtBgxwv9sBxCXbDO" --tenant "daecf046-26ba-44b7-bdd6-032e51085396"
-  echo "Building PingDirectory Image: $RELEASE_TAG"
-  echo "Deploying Ping Directory - DEV"
+  echo "Building $PRODUCT_NAME Image: $RELEASE_TAG"
+  echo "Deploying $PRODUCT_NAME - DEV"
   echo "Logging into ACR"
   az acr login --name baseimage
-  echo "Check if Base image      exists"
+  echo "Check if $PRODUCT_NAME Base image $BASE_IMAGE_TAG exists"
 
   # Check if the base image exists in the Azure Container Registry (ACR)
   if ! az acr repository show-tags --name $ACR_REGISTRY_NAME --repository $BASE_IMAGE_REPOSITORY --output tsv | grep -q "$BASE_IMAGE_TAG"; then
-    echo "Error: Base image pingidentity-base-images/pingdirectory:$BASE_IMAGE_TAG does not exist in the ACR repository.... Exiting The Deployment Pipeline"
-    
+    echo "Error: Base image $BASE_IMAGE_REPOSITORY:$BASE_IMAGE_TAG does not exist in the ACR repository.... Exiting The Deployment Pipeline..."
+    echo "Push $BASE_IMAGE_REPOSITORY:$BASE_IMAGE_TAG to the $ACR_REGISTRY_NAME Registry to continue..."
     
     exit 1
   fi
+  echo "Base image tag $BASE_IMAGE_TAG found. $PRODUCT_NAME Image $RELEASE_TAG will be built using this image..."
 
-  echo "Base image tag $BASE_IMAGE_TAG found."
+
+
+
+
   # echo "Azure login"
   # export APPSETTING_WEBSITE_SITE_NAME='azcli-workaround'
   # az login --identity --username $AZURE_IDENTITY
